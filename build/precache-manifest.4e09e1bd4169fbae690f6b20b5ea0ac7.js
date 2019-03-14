52eb8ae3c5e1d4b2f415c344dbf89752e199df7d
PlayBossWar <titivermeesch@gmail.com>
2019-03-14 12:20:04 +0100

    Deployement version added

---
+++ build/precache-manifest.4e09e1bd4169fbae690f6b20b5ea0ac7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b7b8ec3469a3f14c3b4a704f9731822",
+    "url": "/static/media/portfolio.2b7b8ec3.png"
+  },
+  {
+    "revision": "684c0128453357b3f2ed",
+    "url": "/static/css/main.9d94667b.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f94d90adff6a4f8fec6a",
+    "url": "/static/js/2.379d22b7.chunk.js"
+  },
+  {
+    "revision": "3ce72908ffeed8c50996826acdedf0f2",
+    "url": "/static/media/html-certif.3ce72908.png"
+  },
+  {
+    "revision": "03cd4ce8691ab6e68c813aad927360c9",
+    "url": "/static/media/recipe.03cd4ce8.png"
+  },
+  {
+    "revision": "684c0128453357b3f2ed",
+    "url": "/static/js/main.a9ace560.chunk.js"
+  },
+  {
+    "revision": "34c6450d0bdde7a91ce96f4aa17f0ad8",
+    "url": "/static/media/web-certif.34c6450d.png"
+  },
+  {
+    "revision": "2c1eccc4b96456a648e79f78981c7220",
+    "url": "/static/media/js-certif.2c1eccc4.png"
+  },
+  {
+    "revision": "ba1f5145861ab8c2d4defd4480431755",
+    "url": "/static/media/uml-certif.ba1f5145.PNG"
+  },
+  {
+    "revision": "40faa3c79c82817782a30a0ce95d6e31",
+    "url": "/static/media/resume.40faa3c7.pdf"
+  },
+  {
+    "revision": "b897675c5a422179427209b655a62908",
+    "url": "/index.html"
+  }
+];

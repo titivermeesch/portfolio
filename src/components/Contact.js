3ee0b7c4946d8c4e3ff4495270c7c3dbf97e0295
Tristan Vermeesch <titivermeesch@gmail.com>
2020-02-08 16:38:49 +0100

    update style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 import HeadTitle from './HeadTitle'
+
+const Styled = styled.div`
+	display: grid;
+	justify-content: center;
+	text-align: center;
+	background-color: #3b2349;
+`
 
 const Contact = () => {
 	return (
-		<>
+		<Styled>
 			<HeadTitle>Contact me</HeadTitle>
-			<p>Feel free to contact me if you want to get in touch with me!</p>
-			<h1>titivermeesch@gmail.com</h1>
-		</>
+			<div className="intro">
+				<p>Feel free to contact me if you want to get in touch with me!</p>
+				<h1>titivermeesch@gmail.com</h1>
+			</div>
+		</Styled>
 	)
 }
 

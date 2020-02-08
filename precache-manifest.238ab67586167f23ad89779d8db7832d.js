4a76fd62ce79ee7378a9daa33955318551072c58
Tristan Vermeesch <titivermeesch@gmail.com>
2020-02-08 16:50:26 +0100

    Updates

---
+++ precache-manifest.238ab67586167f23ad89779d8db7832d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1872c818b8948f5104d068b05d2b94ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "de0c445472fa121736fe",
+    "url": "/portfolio/static/css/main.c0114f01.chunk.css"
+  },
+  {
+    "revision": "89ca2305d42297d3d47a",
+    "url": "/portfolio/static/js/2.b885fca1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.b885fca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0c445472fa121736fe",
+    "url": "/portfolio/static/js/main.bfe15839.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "2b7b8ec3469a3f14c3b4a704f9731822",
+    "url": "/portfolio/static/media/portfolio.2b7b8ec3.png"
+  },
+  {
+    "revision": "03cd4ce8691ab6e68c813aad927360c9",
+    "url": "/portfolio/static/media/recipe.03cd4ce8.png"
+  }
+]);

b0af90354621ee3e1c0cc4c096f7b16332c3ce59
Tristan Vermeesch <titivermeesch@gmail.com>
2020-02-08 14:59:04 +0100

    remove serviceworker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-
-serviceWorker.unregister();
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
